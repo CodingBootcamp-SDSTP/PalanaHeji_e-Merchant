6b126a0b0db22a508c388e5988c35d7c3e4da358
hejipalanski <hejipalanski@gmail.com>
2018-06-24 11:51:40 +0800

    Deletes count, destinationCity, destinationState, destinationZipcode and destinationCountry variables. Adds cartId, totalBill, status variables. Modifies counstructors and getDetail method to accommodate added and deleted instance variables.

--- src/Invoice.java
+++ src/Invoice.java
@@ -3,50 +3,43 @@ import java.util.Date;
 
 public class Invoice implements DetailProvider
 {
-	// private static int count = 0;
 	private final String ID;
 	private String customerID;
+	private String cartId;
 	private final String transcationDate;
 	private final String dateOfPayment;
-	private BigDecimal totalBill;
-	private String destinationCity;
-	private String destinationState;
-	private String destinationZipcode;
-	private String destinationCountry;
 	private String destinationAddress;
 	private String cartDetails;
 	private BigDecimal shippingCost;
+	private BigDecimal totalBill;
+	private String status;
 	private boolean isPaid = false;
 
-	public Invoice(String id, String clientId, String total, String address, String zipcode,
-	String city, String country, String state, String cartDetails, String datePaid, String shipCost) {
+	public Invoice(String id, String clientId, String cartId, String transactionDate,
+		String datePaid, String cartDetails, String address, String shipCost, String total, String status) {
 		ID = id;
 		customerID = clientId;
-		totalBill = new BigDecimal(total);
-		shippingCost = new BigDecimal(shipCost);
-		destinationZipcode = zipcode;
-		destinationCity = city;
-		destinationCountry = country;
-		destinationState = state;
-		this.cartDetails = cartDetails;
-		transcationDate = new Date().toString();
+		this.cartId = cartId;
+		transcationDate = transactionDate;
 		dateOfPayment = datePaid;
 		destinationAddress = address;
+		this.cartDetails = cartDetails;
+		shippingCost = new BigDecimal(shipCost);
+		totalBill = new BigDecimal(total);
+		this.status = status;
 	}
 
-	public Invoice(String id, Client c, String datePaid) {
+	public Invoice(String id, Client c, String datePaid, String status) {
 		ID = id;
 		customerID = c.getID();
-		totalBill = c.getCart().getTotalBill();
-		destinationAddress = c.getAddress();
-		destinationZipcode = c.getZipcode();
-		destinationCity = c.getCity();
-		destinationCountry = c.getCountry();
+		cartId = c.getCart().getID();
 		transcationDate = new Date().toString();
 		dateOfPayment = datePaid;
+		destinationAddress = c.getAddress();
 		cartDetails = c.getCart().getDetails();
 		shippingCost =  c.getCart().getShippingCost();
 		totalBill =  c.getCart().getTotalBill();
+		this.status = status;
 	}
 
 	public String getID() {
@@ -73,36 +66,12 @@ public class Invoice implements DetailProvider
 		return(destinationAddress);
 	}
 
-	public void setDestinationCity(String c) {
-		destinationCity = c;
+	public void setOrderStatus(String status) {
+		this.status =status;
 	}
 
-	public String getDestinationCity() {
-		return(destinationCity);
-	}
-
-	public void setDestinationState(String s) {
-		destinationState = s;
-	}
-	
-	public String getDestinationState() {
-		return(destinationState);
-	}
-
-	public void setDestinationZipcode(String d) {
-		destinationZipcode = d;
-	}
-
-	public String getDestinationZipcode() {
-		return(destinationZipcode);
-	}
-
-	public void setDestinationCountry(String c) {
-		destinationCountry = c;
-	}
-
-	public String getDestinationCountry() {
-		return(destinationCountry);
+	public String getOrderStatus() {
+		return(status);
 	}
 
 	public void setCartDetails(String dt) {
@@ -116,16 +85,14 @@ public class Invoice implements DetailProvider
 	public String getDetails() {
 		StringBuilder sb = new StringBuilder(ID);
 			sb.append("~" + customerID);
+			sb.append("~" + cartId);
 			sb.append("~" + transcationDate);
 			sb.append("~" + dateOfPayment);
-			sb.append("~" + destinationCity);
-			sb.append("~" + destinationState);
-			sb.append("~" + destinationZipcode);
-			sb.append("~" + destinationCountry);
 			sb.append("~" + destinationAddress);
 			sb.append("~" + cartDetails);
 			sb.append("~" + shippingCost.toString());
 			sb.append("~" + totalBill.toString());
+			sb.append("~" + status);
 		return(sb.toString());
 	}
 }
